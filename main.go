c6d6a49f6be6ec18eda25dd781c1eb5ed02fff33
Sergio <sergio@sensepost.com>
2021-02-24 10:48:57 +0000

    First commit

--- main.go
+++ main.go
@@ -1,54 +1,53 @@
-package utils
+package main
 
 import (
-    "fmt"
-    "os"
     "log"
-    "net"
-    "bufio"
+    "flag"
+	"errors"
+	"github.com/godscan/core"
 )
 
-/**
- Function that opens and reads the lines in inputFile.
- It return a list of Host structs or an error.
-*/
-func ReadInputFile (inputFile string, ports []Port) ([]Host, error) {
-    var hostList []Host
+func parseArgs() (string, string, string, int, error){
+	var inputFile, outputFile, portList string
+	//var itemId int64
+    var err error
+    threads := flag.Int("threads", 100, "Number of threads (go rouitines) to use.")
+	flag.StringVar(&inputFile, "i", "", "Input file with IP addresses/CIDR network range to scan.")
+	flag.StringVar(&outputFile, "o", "", "Output file name to store the scan results.")
+    flag.StringVar(&portList, "p", "", "Comma-separated list of ports to scan.")
+	flag.Parse()
+	if inputFile == "" || outputFile  == "" || portList == "" {
+		flag.PrintDefaults()
+		err = errors.New("[!!] Wrong parameters.")
+	}
 
-    //Open file
-    file, err := os.Open(inputFile)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
-
-    //Read file line by line
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        ipAddress, ipNet, err := net.ParseCIDR(line)
-        if err != nil {
-            //Line is not in CIDR format
-            //If is not a CIDR, ipNet will be nil
-            ipAddress = net.ParseIP(line)
-        }
-        fmt.Printf("[*] %s - %s\n", ipAddress, ipNet)
-        host := Host{IPAddress: ipAddress, IPNet: ipNet, Ports: ports}
-        hostList = append(hostList, host)
-    }
-    return hostList, nil
+	return inputFile, outputFile, portList, *threads, err
 }
 
-/**
- Function to create the output log file. It uses the log package which provides
- the Logger struct that handles concurrency while writing to a file.
- It return a log.Logger struct or an error.
-*/
-func CreateOutputFile (outputFile string) (*log.Logger, error) {
-    file, err := os.Create(outputFile)
-    log := log.New(file, "", log.LstdFlags|log.Lshortfile)
+func main() {
+	//Parse the arguments
+    inputFile, outputFile, portList, threads, err := parseArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    //Prepare the output file
+    outputFileHandler, err := utils.CreateOutputFile(outputFile)
     if err != nil {
-        return nil, err
+        log.Fatal(err)
     }
-    return log, nil
+
+    //Prepare a list of ports
+    ports, err := utils.ParsePorts(portList)
+    if err !=nil {
+        log.Fatal(err)
+    }
+
+    //Read the hosts to scan
+    hostList, err := utils.ReadInputFile(inputFile, ports)
+    if err != nil {
+        log.Fatal(err)
+    }
+    //Scan the targets
+    utils.ScanListOfHosts(hostList, outputFileHandler, threads)
 }
